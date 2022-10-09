0bd22d5e472cfb9a7134299e569d271b95e65b05
whiteroma <roma.white95@gmail.com>
2022-10-09 16:40:06 +0200

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -11,7 +11,7 @@ import PropTypes from 'prop-types';
 const Statistics = ({ stats, title }) => {
   return (
     <StatsWrapper>
-      <Title>{title ?? ''}</Title>
+     {title &&( <Title>{title}</Title>)}
 
       <StatsList>
         {stats.map(stat => {
